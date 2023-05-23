91528408b01480a48dc392e6d73065e71e489846
Jack Cummings <jack@mudshark.org>
2023-05-22 22:17:16 -0700

    python-remind: 0.15.0 -> 0.18.0

--- pkgs/development/python-modules/remind/default.nix
+++ pkgs/development/python-modules/remind/default.nix
@@ -2,13 +2,13 @@
 
 buildPythonPackage rec {
   pname = "remind";
-  version = "0.15.0";
+  version = "0.18.0";
 
   buildInputs = [ coverage nose ];
   propagatedBuildInputs = [ pytz tzlocal python-dateutil vobject ];
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "28c4635a260dee0fd755fffd6dffd37b2855b09a27a11b23239aace49ec29688";
+    sha256 = "sha256-FB831vi9Vpy6dJ5HROR8cv5CFa9PD4pwFru62Dnd+Kk=";
   };
 }
