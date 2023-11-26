ae53ee34f072e8de63b942411318bf92ec9aabde
Jack Cummings <jack@mudshark.org>
2023-11-25 23:16:40 -0800

    bump

--- pkgs/servers/monitoring/prometheus/iw-exporter.nix
+++ pkgs/servers/monitoring/prometheus/iw-exporter.nix
@@ -2,21 +2,21 @@
 
 buildGoModule rec {
   pname = "iw_exporter";
-  version = "2017-03-29-42ba0399e";
+  version = "2023-09-09-2b17fb9d";
 
   src = fetchFromGitHub {
-    rev = "8d6ae1bf4874504c4b58cdfb2a785885d22b579d";
+    rev = "8a7d1d0f80b31038f54d05b06138ae1f2b17fb9d";
     owner = "jcumming";
     repo = "iw_exporter";
-    sha256 = "sha256:1gz9whmbsvh12sahmahjd9ckfcmvvly97n6vw8c7gj2xls10p6rv";
+    sha256 = "sha256-S9Q4AlEMd6GCpfhd29KiQGigW+6gyCXX0dD03x50WMg=";
   };
 
-  vendorSha256 = "sha256:0mj81xlyqfp3k2lgnjdggnqanksl5bx36byh5y7ir742qlqb7ic7";
+  vendorHash = "sha256-cS9S+yQoLLIC1ZcVDB35S4drYfAOOp6kvT7OuIcOypo=";
 
   meta = with lib; {
     description = "Connected wireless station exporter for Prometheus";
     homepage = "https://github.com/jamessanford/iw_exporter";
-    license = licenses.unfree;
+    license = licenses.wtfpl; # XXX: dunno
     maintainers = with maintainers; [ jcumming ];
     platforms = platforms.linux;
   };
