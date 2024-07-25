382358d2fa8f274291072a22a7ec0a5b8c9ee148
Jack Cummings <jack@mudshark.org>
2024-07-25 07:59:42 -0700

    pull in upstream libxml fixes

--- pkgs/development/libraries/libmusicbrainz/5.x.nix
+++ pkgs/development/libraries/libmusicbrainz/5.x.nix
@@ -1,6 +1,6 @@
 { lib, stdenv, fetchFromGitHub, cmake, neon, libdiscid, libxml2, pkg-config, validatePkgConfig }:
 stdenv.mkDerivation rec {
-  version = "5.1.0+220120-f5a31de";
+  version = "5.1.0+231009-4655b57";
   pname = "libmusicbrainz";
 
   nativeBuildInputs = [ cmake pkg-config validatePkgConfig ];
@@ -9,8 +9,8 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner  = "metabrainz";
     repo   = "libmusicbrainz";
-    sha256 = "sha256-su+7kB5foGMCoVLTZsxt7m1/NT1fEeV6QbIOrtvhz1Q=";
-    rev    = "f5a31ded2d9794e9e27bbdfc197636b3c46b39be";
+    sha256 = "sha256-YJkcRJjXx2obN7bleu2Zz+zxAxcv8lEBLhh6vV6Ikqw=";
+    rev    = "4655b571a70d73d41467091f59c518517c956198";
   };
 
   dontUseCmakeBuildDir=true;
