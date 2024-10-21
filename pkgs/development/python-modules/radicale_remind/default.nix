06628c299d007c8c0fa3a927c7b9e57d9f781ff4
Jack Cummings <jack@mudshark.org>
2024-10-20 18:35:30 -0700

    pickup API changes

--- pkgs/development/python-modules/radicale_remind/default.nix
+++ pkgs/development/python-modules/radicale_remind/default.nix
@@ -9,14 +9,14 @@
 }:
 buildPythonPackage rec {
   pname = "radicale-remind";
-  version = "0.6.0";
+  version = "20240927-cfabc28d11ca72e523ccea943e15b81df96073af";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "jspricke";
     repo = "radicale-remind";
-    rev = "v${version}";
-    hash = "sha256-d4kLUC++mCc73pMXTJLpDIkV6eZ61CGXkeqcaaDgLdo=";
+    rev = "cfabc28d11ca72e523ccea943e15b81df96073af";
+    hash = "sha256-P1Uh00+9RlO2XP9ZILgNLbkFU3z10E72zxnjVuUTtEY=";
   };
 
   nativeBuildInputs = [ radicale ]; # prevent radicale from importing itself
