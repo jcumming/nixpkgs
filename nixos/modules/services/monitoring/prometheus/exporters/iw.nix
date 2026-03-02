ac19ba9c0c97e6789b6c261e1108e3196e9cca44
Jack Cummings <jack@mudshark.org>
2026-03-01 20:41:50 -0800

    renovate prometheus-iw-exporter module

--- nixos/modules/services/monitoring/prometheus/exporters/iw.nix
+++ nixos/modules/services/monitoring/prometheus/exporters/iw.nix
@@ -1,20 +1,26 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  options,
+  ...
+}:
 
-with lib;
-
-let cfg = config.services.prometheus.exporters.iw;
-
-in {
+let
+  cfg = config.services.prometheus.exporters.iw;
+  inherit (lib) concatStringsSep;
+in
+{
   port = 6798;
-
   serviceOpts = {
     path = [ pkgs.iw ];
-
     serviceConfig = {
       RestrictAddressFamilies = [ "AF_UNIX" "AF_NETLINK" ];
 
       ExecStart = ''
-        ${pkgs.prometheus-iw-exporter}/bin/iw_exporter -http ${cfg.listenAddress}:${toString cfg.port}
+        ${pkgs.prometheus-iw-exporter}/bin/iw_exporter \
+          -http ${cfg.listenAddress}:${toString cfg.port} \
+          ${concatStringsSep " \\\n  " cfg.extraFlags}
       '';
     };
   };
