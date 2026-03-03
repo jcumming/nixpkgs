118eb132a1eb8adea6292c1c4f869ce8eacbe890
Jack Cummings <jack@mudshark.org>
2026-03-02 22:02:15 -0800

    derp

--- pkgs/by-name/pr/prometheus-iw-exporter/package.nix
+++ pkgs/by-name/pr/prometheus-iw-exporter/package.nix
@@ -1,21 +1,23 @@
-{ config, pkgs, lib, ... }:
+{ lib, buildGoModule, fetchFromGitHub }:
 
-with lib;
+buildGoModule rec {
+  pname = "iw_exporter";
+  version = "2023-09-09-2b17fb9d";
 
-let cfg = config.services.prometheus.exporters.iw;
+  src = fetchFromGitHub {
+    rev = "8a7d1d0f80b31038f54d05b06138ae1f2b17fb9d";
+    owner = "jcumming";
+    repo = "iw_exporter";
+    sha256 = "sha256-S9Q4AlEMd6GCpfhd29KiQGigW+6gyCXX0dD03x50WMg=";
+  };
 
-in {
-  port = 6798;
+  vendorHash = "sha256-cS9S+yQoLLIC1ZcVDB35S4drYfAOOp6kvT7OuIcOypo=";
 
-  serviceOpts = {
-    path = [ pkgs.iw ];
-
-    serviceConfig = {
-      RestrictAddressFamilies = [ "AF_UNIX" "AF_NETLINK" ];
-
-      ExecStart = ''
-        ${pkgs.prometheus-iw-exporter}/bin/iw_exporter -http ${cfg.listenAddress}:${toString cfg.port}
-      '';
-    };
+  meta = with lib; {
+    description = "Connected wireless station exporter for Prometheus";
+    homepage = "https://github.com/jamessanford/iw_exporter";
+    license = licenses.wtfpl; # XXX: dunno
+    maintainers = with maintainers; [ jcumming ];
+    platforms = platforms.linux;
   };
 }
